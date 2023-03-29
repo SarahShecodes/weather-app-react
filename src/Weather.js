65f65d969c87ca0870e3d8a1bf36b602676d86a7
SarahShecodes <123887765+SarahShecodes@users.noreply.github.com>
2023-03-29 18:42:37 +0100

    creating props to receive city

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./Weather.css";
 import { MagnifyingGlass } from "react-loader-spinner";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -64,8 +64,8 @@ export default function Weather() {
     );
   } else {
     const apiKey = "094780c710fa4efd669f0df8c3991927";
-    let city = "Madrid";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return (
