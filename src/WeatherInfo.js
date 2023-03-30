863d40b10836e3b7d3927f12b2317bd308a155c3
SarahShecodes <123887765+SarahShecodes@users.noreply.github.com>
2023-03-30 17:45:59 +0100

    new component (WeatherTemperature)
    function to convert Celsius to Fahrenheit temp

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,15 +15,20 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} size={52} />
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
 
-          <span className="temperature">{props.data.temperature}</span>
-          <span className="unit">ºC </span>
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity:{props.data.humidity}%</li>
-            <li>Wind: {props.data.wind}km/h</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
